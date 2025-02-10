d120fbd967847c6c6fc98f06b20320d570147496
shantanu gote <84043270+gotesgan@users.noreply.github.com>
2025-02-10 22:27:38 +0530

    Initial Nx monorepo with React and Node.js

---
+++ apps/website/src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import * as ReactDOM from 'react-dom/client';
+import App from './app/app';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
